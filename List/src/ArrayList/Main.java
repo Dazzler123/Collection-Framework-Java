3f7fdf56092bdcbe48178d20ec2093d218e58fb6
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-18 20:59:48 +0530

    Content updated

--- List/src/ArrayList/Main.java
+++ List/src/ArrayList/Main.java
@@ -1,7 +1,9 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> list = new ArrayList<>();
     }
 }
