b9df253478c77395ab25490f0e46fb28636c85b3
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-18 20:14:40 +0530

    Initial Commit

---
+++ List/src/LinkedList/Main.java
@@ -0,0 +1,13 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 18-Jun-2023
+ * Time : 7:45 PM
+ * Project Name : Collection-Framework-Practices
+ */
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
