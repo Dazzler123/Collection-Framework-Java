1120eaa286c078b1372f1dc29976c3f4a2a47727
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-18 21:17:47 +0530

    Content updated

--- List/src/ArrayList/Main.java
+++ List/src/ArrayList/Main.java
@@ -3,7 +3,17 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class Main {
+
+    /*
+    ArrayList is an implementation of the List interface. It uses a dynamic array to store the duplicate elements
+    of the different data types.
+
+    The ArrayList class maintains the insertion order and is non-synchronized.
+    The elements stored in the ArrayList class can be randomly accessed.
+     */
+
     public static void main(String[] args) {
+
         ArrayList<String> list = new ArrayList<>();
     }
 }
