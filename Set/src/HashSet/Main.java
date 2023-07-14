37c7437eae59beafd77937db651740ae5a649269
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-14 13:40:44 +0530

    Content updated

--- Set/src/HashSet/Main.java
+++ Set/src/HashSet/Main.java
@@ -1,7 +1,17 @@
 package HashSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<String> hashset = new HashSet<>();
+        hashset.add("Dazzler");
+        hashset.add("Marky");
+        hashset.add("Abby");
+
+        System.out.println(hashset.contains("Dazzler"));
+
+        System.out.println(hashset);
     }
 }
